b4fa250954b11ba6e0abf4a961fb9304c9b33538
Zhen Wang <15000059524@163.com>
2024-03-23 22:46:21 +1100

    product price page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
+    # This is the root path route ("/")
+    # to be used as the dashboard
   end
 end
